ab6bd988e80294722a20837f5b78e1526f165688
Andres Oviedo <andresoviedo@gmail.com>
2016-03-03 18:43:24 +0100

    Send page url to google analytics

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -10,4 +10,4 @@
 	m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 ga('create', 'UA-66312832-1', 'auto');
-ga('send', 'pageview');
+ga('send', 'pageview', location.pathname);
