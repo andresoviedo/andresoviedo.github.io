157d37e1807975fc8fbb376a7fa6b13872e161db
andresoviedo <andresoviedo@gmail.com>
2018-07-30 00:42:31 +0200

    cleanup

--- js/blog.js
+++ js/blog.js
@@ -1,26 +1,36 @@
-// Blog
-google.load("feeds", "1");
-function initialize() {
-	var feed = new google.feeds.Feed(
-			"http://andresoviedo82.blogspot.com/feeds/posts/default");
-	feed.load(function(result) {
-		if (!result.error) {
+/*$(document).ready(function() {
+	//feed to parse
+	var feed = "blog/blog.rss";
+	
+	$.ajax(feed, {
+		accepts:{
+			xml:"application/rss+xml"
+		},
+		dataType:"xml",
+		success:function(data) {
+			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
 			var container = document.getElementById("feed");
-			for (var i = 0; i < result.feed.entries.length; i++) {
-				var entry = result.feed.entries[i];
+			$(data).find("item").each(function () { // or "item" or whatever suits your feed
+				var el = $(this);
+				console.log("------------------------");
+				console.log("title      : " + el.find("title").text());
+				console.log("link       : " + el.find("link").text());
+				console.log("description: " + el.find("description").text());
 
 				var h4 = document.createElement("h6");
 				var a = document.createElement("a");
-				a.appendChild(document.createTextNode(entry.title));
-				a.href = entry.link;
+				a.appendChild(document.createTextNode(el.find("title").text()));
+				a.href = el.find("link").text();
 				h4.appendChild(a);
 				container.appendChild(h4);
 
 				var p = document.createElement("p");
-				p.appendChild(document.createTextNode(entry.contentSnippet));
+				p.innerHTML = el.find("description").text();
 				container.appendChild(p);
-			}
-		}
+			});
+	
+
+		}	
 	});
-}
-google.setOnLoadCallback(initialize);
+	
+});*/
